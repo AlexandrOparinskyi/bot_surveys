7dc743bc50d23ccc23c39b80783a138495c46d83
Александр <opar1297@mail.ru>
2025-05-20 20:10:24 +0300

    del cors

--- database/admin.py
+++ database/admin.py
@@ -27,15 +27,8 @@ from database.schemas import SurveyBase
 app = FastAPI()
 admin = Admin(app, engine)
 UPLOAD_FOLDER = "/app/uploads"
-templates = Jinja2Templates(directory="/app/templates")
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Разрешить всем!
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-app.mount("/static", StaticFiles(directory="/app/static"), name="static")
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.post("/create_poll", status_code=201)
